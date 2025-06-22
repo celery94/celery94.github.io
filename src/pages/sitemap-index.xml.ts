89af7ab4a6308e32a141527f12cb06baeba0c053
Celery Liu <111548185+Celery-Liu@users.noreply.github.com>
2025-06-22 19:40:18 +0800

    feat: refactor sitemap generation and remove deprecated files

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -1,34 +1,84 @@
 import type { APIRoute } from "astro";
+import type { CollectionEntry } from "astro:content";
 import { SITE } from "@/config";
 import { getCollection } from "astro:content";
+import getSortedPosts from "@/utils/getSortedPosts";
+import getUniqueTags from "@/utils/getUniqueTags";
 
 export const GET: APIRoute = async () => {
   const posts = await getCollection("blog");
+  const sortedPosts = getSortedPosts(posts);
+  const tags = getUniqueTags(posts);
   const lastModified = new Date().toISOString();
-  
-  // Get the latest post date for posts sitemap
-  const latestPostDate = posts.reduce((latest, post) => {
-    const postDate = post.data.modDatetime || post.data.pubDatetime;
-    return postDate > latest ? postDate : latest;
-  }, new Date(0));
 
-  const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-    <loc>${SITE.website}sitemap-0.xml</loc>
-    <lastmod>${lastModified}</lastmod>
-  </sitemap>
-  <sitemap>
-    <loc>${SITE.website}sitemap-posts.xml</loc>
-    <lastmod>${latestPostDate.toISOString()}</lastmod>
-  </sitemap>
-  <sitemap>
-    <loc>${SITE.website}sitemap-tags.xml</loc>
-    <lastmod>${lastModified}</lastmod>
-  </sitemap>
-</sitemapindex>`;
+  // Static pages URLs
+  const staticPages = [
+    {
+      url: `${SITE.website}`,
+      lastmod: lastModified,
+      changefreq: "daily",
+      priority: "1.0"
+    },
+    {
+      url: `${SITE.website}about/`,
+      lastmod: lastModified,
+      changefreq: "monthly",
+      priority: "0.8"
+    },
+    {
+      url: `${SITE.website}search/`,
+      lastmod: lastModified,
+      changefreq: "weekly",
+      priority: "0.7"
+    },
+    {
+      url: `${SITE.website}tags/`,
+      lastmod: lastModified,
+      changefreq: "weekly",
+      priority: "0.7"
+    },
+    {
+      url: `${SITE.website}archives/`,
+      lastmod: lastModified,
+      changefreq: "weekly", 
+      priority: "0.7"
+    }
+  ];
 
-  return new Response(sitemapIndex, {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${staticPages
+  .map(page => `  <url>
+    <loc>${page.url}</loc>
+    <lastmod>${page.lastmod}</lastmod>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>
+  </url>`)
+  .join("\n")}
+${sortedPosts
+  .map((post: CollectionEntry<"blog">) => {
+    const lastmod = post.data.modDatetime || post.data.pubDatetime;
+    return `  <url>
+    <loc>${SITE.website}posts/${post.id}/</loc>
+    <lastmod>${lastmod.toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  })
+  .join("\n")}
+${tags
+  .map((tagObj: { tag: string; tagName: string }) => {
+    return `  <url>
+    <loc>${SITE.website}tags/${tagObj.tag}/</loc>
+    <lastmod>${lastModified}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.6</priority>
+  </url>`;
+  })
+  .join("\n")}
+</urlset>`;
+
+  return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml",
     },
